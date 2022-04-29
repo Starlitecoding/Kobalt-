8f7e0b15839b761a7a30d4a7cbabb5d90c570bef
sayoneone <74616403+sayoneone@users.noreply.github.com>
2022-04-30 03:41:03 +0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.d1e71f7f932b49035ec7b67377db0e21.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417bcb76fbd974e2565761aba7757600",
+    "url": "/Kobalt-/tree/prod/index.html"
+  },
+  {
+    "revision": "5769d690a745d6564221",
+    "url": "/Kobalt-/tree/prod/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "6f97f330eb3d14428436",
+    "url": "/Kobalt-/tree/prod/static/css/4.4a590464.chunk.css"
+  },
+  {
+    "revision": "682c7dadf817d30f3f80",
+    "url": "/Kobalt-/tree/prod/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "57a031b3f1a17d8f3245",
+    "url": "/Kobalt-/tree/prod/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "29b0c12bf2d10fba4487",
+    "url": "/Kobalt-/tree/prod/static/css/main.d4bdde6e.chunk.css"
+  },
+  {
+    "revision": "5769d690a745d6564221",
+    "url": "/Kobalt-/tree/prod/static/js/0.8c0ec5c7.chunk.js"
+  },
+  {
+    "revision": "6dcbbc1ad5c0bbc6639d",
+    "url": "/Kobalt-/tree/prod/static/js/3.6f739876.chunk.js"
+  },
+  {
+    "revision": "6f97f330eb3d14428436",
+    "url": "/Kobalt-/tree/prod/static/js/4.522df275.chunk.js"
+  },
+  {
+    "revision": "682c7dadf817d30f3f80",
+    "url": "/Kobalt-/tree/prod/static/js/5.5728056f.chunk.js"
+  },
+  {
+    "revision": "8e8dc7b3fcebc6a70fd7",
+    "url": "/Kobalt-/tree/prod/static/js/6.f1026129.chunk.js"
+  },
+  {
+    "revision": "57a031b3f1a17d8f3245",
+    "url": "/Kobalt-/tree/prod/static/js/7.5ca0828a.chunk.js"
+  },
+  {
+    "revision": "29b0c12bf2d10fba4487",
+    "url": "/Kobalt-/tree/prod/static/js/main.798e64ae.chunk.js"
+  },
+  {
+    "revision": "077d2bd5b37526d427c3",
+    "url": "/Kobalt-/tree/prod/static/js/runtime~main.6199a939.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/Kobalt-/tree/prod/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "3e47c4fe1a372dee441d9420065690e2",
+    "url": "/Kobalt-/tree/prod/static/media/camomile_blurred.3e47c4fe.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/Kobalt-/tree/prod/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "53c159c4b6ec6d114e61c0addea6c2b7",
+    "url": "/Kobalt-/tree/prod/static/media/telegram-logo.53c159c4.svg"
+  }
+]);

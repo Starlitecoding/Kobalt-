b4e8580b0e9091ff3e85179b3fdcaed1a1da64a3
sayoneone <74616403+sayoneone@users.noreply.github.com>
2022-04-30 03:34:38 +0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.435ac3d0f4dccabb6a88b9842e7bc2a8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d517d34b6dfb93a0adb295ff3da8e5",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/index.html"
+  },
+  {
+    "revision": "5769d690a745d6564221",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "4de2859f6f4a1d9a64c6",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/css/4.af9928f7.chunk.css"
+  },
+  {
+    "revision": "682c7dadf817d30f3f80",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "57a031b3f1a17d8f3245",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/css/7.751a6c49.chunk.css"
+  },
+  {
+    "revision": "bf188c8fe9af9d04cae7",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/css/main.d4bdde6e.chunk.css"
+  },
+  {
+    "revision": "5769d690a745d6564221",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/0.8c0ec5c7.chunk.js"
+  },
+  {
+    "revision": "6dcbbc1ad5c0bbc6639d",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/3.6f739876.chunk.js"
+  },
+  {
+    "revision": "4de2859f6f4a1d9a64c6",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/4.2b3f34ca.chunk.js"
+  },
+  {
+    "revision": "682c7dadf817d30f3f80",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/5.5728056f.chunk.js"
+  },
+  {
+    "revision": "8e8dc7b3fcebc6a70fd7",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/6.f1026129.chunk.js"
+  },
+  {
+    "revision": "57a031b3f1a17d8f3245",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/7.5ca0828a.chunk.js"
+  },
+  {
+    "revision": "bf188c8fe9af9d04cae7",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/main.e4850aaa.chunk.js"
+  },
+  {
+    "revision": "ee061978a2995e9306a8",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/js/runtime~main.d1818615.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "3e47c4fe1a372dee441d9420065690e2",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/media/camomile_blurred.3e47c4fe.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "53c159c4b6ec6d114e61c0addea6c2b7",
+    "url": "/Starlitecoding/Kobalt-/tree/prods/static/media/telegram-logo.53c159c4.svg"
+  }
+]);
